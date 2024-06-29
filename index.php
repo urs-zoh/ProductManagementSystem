71c0af57abc700c28916ca24b6a1e54859ce0469
Zakhar Vashchenko <69220793+urs-zoh@users.noreply.github.com>
2024-06-29 20:04:07 +0200

    added price tags and the sort now works by the price

--- index.php
+++ index.php
@@ -1,9 +1,8 @@
-<!doctype html>
+<!DOCTYPE html>
 <html lang="en">
 <head>
     <meta charset="UTF-8">
-    <meta name="viewport"
-          content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
+    <meta name="viewport" content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
     <title>Product Management System</title>
     <link rel="stylesheet" href="./css/style.css">
@@ -13,21 +12,19 @@
 <form method="POST" action="">
     <div class="controls">
         <!-- Search -->
-        <form method="POST" action="">
-            <input type="text" name="search" placeholder="Search Product">
-            <select name="searchType">
-                <option value="binary">Binary Search</option>
-                <option value="linear">Linear Search</option>
-            </select>
-            <button type="submit" name="searchSubmit">Search</button>
-        </form>
-        
+        <input type="text" name="search" placeholder="Search Product">
+        <select name="searchType">
+            <option value="binary">Binary Search</option>
+            <option value="linear">Linear Search</option>
+        </select>
+        <button type="submit" name="searchSubmit">Search</button>
+
         <hr>
-        
+
         <!-- Sort -->
         <button class="sort" name="sort" value="quick" type="submit">Quick Sort</button>
         <button class="sort" name="sort" value="bubble" type="submit">Bubble Sort</button>
-        
+
         <!-- Modify -->
         <button class="modify" name="flip" value="flip" type="submit">Flip List</button>
         <button class="modify" name="changeCase" value="changeCase" type="submit">Change Case</button>
@@ -43,13 +40,29 @@
     <button type="submit" name="login">Login</button>
 </form>
 <?php
-// Product array
+// Product array with prices
 $products = [
-    "Apple" => ["iPhone 6s", "iPhone 10", "iPhone 14", "iPhone 15 Pro Max"],
-    "Samsung" => ["Galaxy S10", "Galaxy S20", "Galaxy S6", "Galaxy S21"],
-    "Nokia" => ["3310", "N-Gage", "Lumia"],
-    "Sony" => ["Xperia", "PlayStation", "Walkman", "Bravia"],
-    "LG" => ["G2", "G3", "G4"]
+    "Apple" => [
+        ["name" => "iPhone 6s", "price" => 300],
+        ["name" => "iPhone 10", "price" => 600],
+        ["name" => "iPhone 14", "price" => 1000],
+        ["name" => "iPhone 8", "price" => 400]
+    ],
+    "Samsung" => [
+        ["name" => "Galaxy S20", "price" => 800],
+        ["name" => "Galaxy S10", "price" => 500],
+        ["name" => "Galaxy S9", "price" => 400]
+    ],
+    "Nokia" => [
+        ["name" => "Nokia 3310", "price" => 50],
+        ["name" => "Nokia 8110", "price" => 100],
+        ["name" => "Nokia 3210", "price" => 30]
+    ],
+    "Sony" => [
+        ["name" => "Xperia 1", "price" => 700],
+        ["name" => "Xperia 10", "price" => 400],
+        ["name" => "Xperia 5", "price" => 500]
+    ]
 ];
 
 // Handle login
@@ -58,17 +71,18 @@ if (isset($_POST['login'])) {
     $password = $_POST['password'];
     if ($username === 'moderator' && $password === 'password') {
         echo "<div id='productManagement' style='text-align: center;'>
-                    <form method='POST' action=''>
-                        <input type='text' name='manufacturer' placeholder='Manufacturer' required>
-                        <input type='text' name='product' placeholder='Product' required>
-                        <button type='submit' name='addProduct'>Add Product</button>
-                    </form>
-                    <form method='POST' action=''>
-                        <input type='text' name='manufacturer' placeholder='Manufacturer' required>
-                        <input type='text' name='product' placeholder='Product' required>
-                        <button type='submit' name='removeProduct'>Remove Product</button>
-                    </form>
-                </div>";
+                        <form method='POST' action=''>
+                            <input type='text' name='manufacturer' placeholder='Manufacturer' required>
+                            <input type='text' name='product' placeholder='Product' required>
+                            <input type='number' name='price' placeholder='Price' required>
+                            <button type='submit' name='addProduct'>Add Product</button>
+                        </form>
+                        <form method='POST' action=''>
+                            <input type='text' name='manufacturer' placeholder='Manufacturer' required>
+                            <input type='text' name='product' placeholder='Product' required>
+                            <button type='submit' name='removeProduct'>Remove Product</button>
+                        </form>
+                    </div>";
     } else {
         echo "<p style='color: red; text-align: center;'>Invalid login!</p>";
     }
@@ -78,15 +92,19 @@ if (isset($_POST['login'])) {
 if (isset($_POST['addProduct'])) {
     $manufacturer = $_POST['manufacturer'];
     $product = $_POST['product'];
-    $products[$manufacturer][] = $product;
+    $price = $_POST['price'];
+    $products[$manufacturer][] = ["name" => $product, "price" => $price];
 }
 
 // Handle removing product
 if (isset($_POST['removeProduct'])) {
     $manufacturer = $_POST['manufacturer'];
     $product = $_POST['product'];
-    if (($key = array_search($product, $products[$manufacturer])) !== false) {
-        unset($products[$manufacturer][$key]);
+    foreach ($products[$manufacturer] as $key => $item) {
+        if ($item['name'] === $product) {
+            unset($products[$manufacturer][$key]);
+            break;
+        }
     }
 }
 
@@ -102,7 +120,7 @@ if (isset($_POST['sort'])) {
     }
 }
 
-// Handle search
+// Handle search TODO: fix search
 if (isset($_POST['searchSubmit']) && isset($_POST['searchType'])) {
     $searchType = $_POST['searchType'];
     $searchValue = $_POST['search'];
@@ -115,7 +133,7 @@ if (isset($_POST['searchSubmit']) && isset($_POST['searchType'])) {
             }
         } elseif ($searchType == 'linear') {
             foreach ($productArray as $product) {
-                if ($product == $searchValue) {
+                if ($product['name'] == $searchValue) {
                     $searchResults[$manufacturer][] = $product;
                     break;
                 }
@@ -128,7 +146,7 @@ if (isset($_POST['searchSubmit']) && isset($_POST['searchType'])) {
             echo "<h3>$manufacturer</h3>";
             echo "<ul>";
             foreach ($results as $result) {
-                echo "<li>$result</li>";
+                echo "<li>{$result['name']} - \${$result['price']}</li>";
             }
             echo "</ul>";
         }
@@ -143,10 +161,10 @@ function binarySearch($array, $x) {
     $high = count($array) - 1;
     while ($low <= $high) {
         $mid = floor(($low + $high) / 2);
-        if ($array[$mid] == $x) {
+        if ($array[$mid]['name'] == $x) {
             return true;
         }
-        if ($array[$mid] < $x) {
+        if ($array[$mid]['name'] < $x) {
             $low = $mid + 1;
         } else {
             $high = $mid - 1;
@@ -166,9 +184,7 @@ if (isset($_POST['flip'])) {
 if (isset($_POST['changeCase'])) {
     foreach ($products as &$productArray) {
         foreach ($productArray as &$product) {
-            if (is_string($product)) {
-                $product = strtoupper($product);
-            }
+            $product['name'] = strtoupper($product['name']);
         }
     }
 }
@@ -176,31 +192,35 @@ if (isset($_POST['changeCase'])) {
 // Display products or search results
 if (!empty($searchResults)) {
     echo "<h2>Search Results</h2>";
-    foreach ($searchResults as $key => $value) {
-        echo "<h3>$key</h3>";
-        echo "<ul><li>$value</li></ul>";
+    foreach ($searchResults as $manufacturer => $results) {
+        echo "<h3>$manufacturer</h3>";
+        echo "<ul>";
+        foreach ($results as $result) {
+            echo "<li>{$result['name']} - {$result['price']}€</li>";
+        }
+        echo "</ul>";
     }
 } else {
     ksort($products);
-    foreach ($products as $key => $value) {
-        echo "<h2>$key</h2>";
+    foreach ($products as $manufacturer => $productArray) {
+        echo "<h2>$manufacturer</h2>";
         echo "<ul>";
-        foreach ($value as $product) {
-            echo "<li>$product</li>";
+        foreach ($productArray as $product) {
+            echo "<li>{$product['name']} - {$product['price']}€</li>";
         }
         echo "</ul>";
     }
 }
 
 // Quick Sort function
-function quicksort(&$array) {
+function quicksort($array) {
     if(count($array) < 2) return $array;
     $left = $right = [];
     reset($array);
     $pivot_key = key($array);
     $pivot = array_shift($array);
     foreach($array as $k => $v) {
-        if($v < $pivot)
+        if($v['price'] < $pivot['price'])
             $left[$k] = $v;
         else
             $right[$k] = $v;
@@ -213,7 +233,7 @@ function bubbleSort(&$array) {
     $n = count($array);
     for ($i = 0; $i < $n - 1; $i++) {
         for ($j = 0; $j < $n - $i - 1; $j++) {
-            if ($array[$j] > $array[$j + 1]) {
+            if ($array[$j]['price'] > $array[$j + 1]['price']) {
                 $temp = $array[$j];
                 $array[$j] = $array[$j + 1];
                 $array[$j + 1] = $temp;
